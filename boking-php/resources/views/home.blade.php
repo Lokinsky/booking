5f4320268f97f30cff14ab5420dacfa0e29f5034
Lokinsky <wf.pbvova2030@icloud.com>
2020-10-22 16:38:11 +0300

    )

--- boking-php/resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master')
-@section('title',$title[app()->getLocale()])
-@section('content')
-    <div id="app">
-    
-    </div>
-    <script src="js/booking.app.js"></script>
-@endsection
-
